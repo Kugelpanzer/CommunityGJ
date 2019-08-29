80245e799c735b16ab9baf7e62b1419fe5006b2f
Kugelpanzer <djolemlad@gmail.com>
2019-08-29 22:57:49 +0200

    Mali Commit

--- GameIsALiar/Assets/DrawGrid.cs
+++ GameIsALiar/Assets/DrawGrid.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class DrawGrid : MonoBehaviour
 {
-    //[System.Serializable]
+    [System.Serializable]
     public struct IdObj
     {
         public GameObject gj;
@@ -12,9 +12,11 @@ public class DrawGrid : MonoBehaviour
     }
     BoardClass br;
 
+    public GameObject bakcgroundObj;
     public List<IdObj> ConvertList = new List<IdObj>();
     Dictionary<int,GameObject> DictList= new Dictionary<int,GameObject>();
 
+    List<GameObject> ExistingObjects = new List<GameObject>();
     //public GridObject[,] Board;
     // Start is called before the first frame update
     
@@ -30,15 +32,43 @@ public class DrawGrid : MonoBehaviour
         {
             DictList.Add(io.id, io.gj);
         }
-
+        SpawnBackground();
     }
 
     public void UpdateGrid()
     {
+
+        if(ExistingObjects.Count!=0)
+            for (int i = 0; i < ExistingObjects.Count - 1; i++)
+            {
+                 Destroy(ExistingObjects[i]);
+            }
+        Debug.Log(ExistingObjects.Count);
         for (int i = 0; i < br.GameBoard.GetLength(0); i++)
             for (int j = 0; j < br.GameBoard.GetLength(1); j++)
             {
-                
+
+                //gj = Instantiate(DictList[1]);
+                if (DictList.ContainsKey(br.GameBoard[i, j]))
+                {
+                    ExistingObjects[ExistingObjects.Count - 1] = Instantiate(DictList[br.GameBoard[i, j]]);
+                    Sprite spr = ExistingObjects[ExistingObjects.Count - 1].GetComponent<SpriteRenderer>().sprite;
+                    ExistingObjects[ExistingObjects.Count - 1].transform.position = new Vector2(j * (spr.rect.width / 100), i * (spr.rect.height / 100));
+                }
+            }
+
+    }
+    public void SpawnBackground()
+    {
+        GameObject gj;
+
+        for (int i = 0; i < br.GameBoard.GetLength(0); i++)
+            for (int j = 0; j < br.GameBoard.GetLength(1); j++)
+            {
+
+                gj = Instantiate(DictList[1]);
+                Sprite spr = gj.GetComponent<SpriteRenderer>().sprite;
+                gj.transform.position = new Vector2(j * (spr.rect.width / 100), i * (spr.rect.height / 100));
             }
 
     }
